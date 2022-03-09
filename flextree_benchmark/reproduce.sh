14d8c026712af43eb5145d462baa9d3eb804f4eb
Chen Chen <mail@roychan.org>
2022-03-09 10:42:35 -0600

    fix experiment

--- flextree_benchmark/reproduce.sh
+++ flextree_benchmark/reproduce.sh
@@ -3,18 +3,18 @@
 echo Left rows: 10^5 insert and 10^8 append/lookup/range
 
 make -B M=j INSERT=100000lu REGULAR=100000000lu 1>/dev/null
-./test.out
+taskset -c 0 ./test.out
 make -B M=j INSERT=100000lu REGULAR=100000000lu BTREE=y 1>/dev/null
-./test.out
+taskset -c 0 ./test.out
 make -B M=j INSERT=100000lu REGULAR=100000000lu ARRAY=y 1>/dev/null
-./test.out
+taskset -c 0 ./test.out
 
 echo
 echo Right rows: 10^6 insert and 10^9 append/lookup/range
 
 make -B M=j INSERT=1000000lu REGULAR=1000000000lu 1>/dev/null
-./test.out
+taskset -c 0 ./test.out
 make -B M=j INSERT=1000000lu REGULAR=1000000000lu BTREE=y 1>/dev/null
-./test.out
+taskset -c 0 ./test.out
 make -B M=j INSERT=1000000lu REGULAR=1000000000lu ARRAY=y 1>/dev/null
-./test.out
+taskset -c 0 ./test.out
